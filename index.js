261b7cb7871371a94364cde26209ce03392eee43
JayceTDE <jaycemp@gmail.com>
2013-04-26 14:38:38 -0600

    Remove the UI aspect and move it to a new component

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var io = require('socket.io')
 	, Emitter = require('emitter')
-	, classes = require('classes')
 	, inherit = require('inherit')
 ;
 
@@ -11,9 +10,6 @@ function Conversation(targetInfo, parent) {
 	this.targetInfo = targetInfo;
 
 	this.parent = parent;
-
-	this.el = document.createElement('div');
-	classes(this.el).add('chat-conversation');
 
 }
 
@@ -39,21 +35,10 @@ function Client(host, options) {
 
 	this.socket = io(host + '/chat');
 	
-	this.socket.io
-		.on('reconnect', this._reconnect)
-		.on('reconnect_failed', this._reconnect_failed)
-	;
-
 	this.socket
-		.on('connect', this._connect)
-		.on('disconnect', this._disconnect)
 		.on('msg', this._receiveMessage)
 		.on('dist', this._distribute)
 	;
-
-	this.el = document.createElement('div');
-	this.el.id = options.containerId || 'chat-client';
-	classes(this.el).add('chat-disconnected');
 
 	this.conversations = {};
 
@@ -63,30 +48,6 @@ inherit(Client, Emitter);
 
 // Expose Conversation to allow for prototype manipulation
 Client.Conversation = Conversation;
-
-Client.prototype._connect = function () {
-
-	classes(this.el).remove('chat-disconnected');
-
-};
-
-Client.prototype._disconnect = function () {
-
-	classes(this.el).add('chat-disconnected');
-
-};
-
-Client.prototype._reconnect = function () {
-
-	classes(this.el).add('chat-reconnecting');
-
-};
-
-Client.prototype._reconnect_failed = function () {
-
-	classes(this.el).remove('chat-reconnecting').add('chat-disconnected');
-
-};
 
 Client.prototype._receiveMessage = function (targetInfo, msg) {
 
